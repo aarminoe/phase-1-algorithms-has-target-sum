ffd91afdc5bc2ec829e4faca90b4340b4acdd99f
Aaron Noe <aarminoe@gmail.com>
2022-05-07 21:08:38 -0500

    commit tree a4a887aacaf12caaf583b438dbfc8a584f56e192

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //iterate through array
+  for (let i = 0; i <= array.length; i++) {
+    //create variable to check if the target number minus that array number is 
+    //equal to another number in the array
+    const numInArray = target - array[i]
+    //iterate through the rest of the numbers in the array using i + 1
+    for (let x = i + 1; x < array.length; x++) {
+      if (numInArray === array[x]) {
+        return true
+      }
+    }
+  }
+  return false
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(2n)
+  O(n)
 */
 
 /* 
   Add your pseudocode here
+  in function
 */
 
 /*
   Add written explanation of your solution here
+  Check if any 2 numbers of a given array is equal to the target.
 */
 
 // You can run `node index.js` to view these console logs
